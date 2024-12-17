b6a0d252bafb0e05e5ec17e0bd7b4513b9972157
rise <quinnyan@rdchina.net>
2024-12-17 19:02:19 +0800

    区分环境

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   },
